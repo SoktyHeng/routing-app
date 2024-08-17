d64ffb9535ab3df47e6897c6a405e12905ce3024
SoktyHeng <101002552+SoktyHeng@users.noreply.github.com>
2024-08-18 01:09:01 +0700

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -18,40 +18,45 @@ import EditContact, {
 import { action as destroyAction } from "./routes/destroy";
 import Index from "./routes/index";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root />,
+      errorElement: <ErrorPage />,
+      loader: rootLoader,
+      action: rootAction,
+      children: [
+        {
+          errorElement: <ErrorPage />,
+          children: [
+            { index: true, element: <Index /> },
+            {
+              path: "contacts/:contactId",
+              element: <Contact />,
+              loader: contactLoader,
+              action: contactAction,
+            },
+            {
+              path: "contacts/:contactId/edit",
+              element: <EditContact />,
+              loader: contactLoader,
+              action: editAction,
+            },
+            {
+              path: "contacts/:contactId/destroy",
+              action: destroyAction,
+              errorElement: <div>Oops! There was an error.</div>,
+            },
+          ]
+        }
+      ],
+    },
+  ], 
   {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    loader: rootLoader,
-    action: rootAction,
-    children: [
-      {
-        errorElement: <ErrorPage />,
-        children: [
-          { index: true, element: <Index /> },
-          {
-            path: "contacts/:contactId",
-            element: <Contact />,
-            loader: contactLoader,
-            action: contactAction,
-          },
-          {
-            path: "contacts/:contactId/edit",
-            element: <EditContact />,
-            loader: contactLoader,
-            action: editAction,
-          },
-          {
-            path: "contacts/:contactId/destroy",
-            action: destroyAction,
-            errorElement: <div>Oops! There was an error.</div>,
-          },
-        ]
-      }
-    ],
-  },
-]);
+    basename: "/routing-app",
+  }
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
